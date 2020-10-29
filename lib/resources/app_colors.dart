92fcf0a0bd9fc03bdc0a58f82e04f0b80ec40456
rahul <shakti@excellencetechnologies.in>
2020-10-29 19:22:29 +0530

    Feedback

---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color BACKGROUND_COLOR = Color(0xff001C37); //001C37
+  static const Color THEME_COLOR = Color(0xffF1F6FA); //F1F6FA
+  static const Color BTN_COLOR = Color(0xff4169E1); //4169E1
+  static const Color LIGHT_BLUE = Color(0xffDDF5FC); // DDF5FC
+  static const Color LIGHT_GREEN = Color(0xff67B6AE); //67B6AE
+}
